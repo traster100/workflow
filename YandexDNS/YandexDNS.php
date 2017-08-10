1daadb564b859187aa7354a411766872b53b0cdd
Alexey Gallyamov <traster100@gmail.com>
2017-08-10 17:56:02 +0300

    Add files via upload

---
+++ YandexDNS/YandexDNS.php
@@ -0,0 +1,186 @@
+<?php
+
+/*
+Создание регистранта на Яндексе:
+
+Зарегистрировать приложение на сервисе OAuth: https://oauth.yandex.ru/client/new
+ID: id123
+Пароль: pass123
+Callback URL: https://oauth.yandex.ru/verification_code
+
+Создать учетную запись регистратора на странице управления регистратором: https://pddimp.yandex.ru/api2/registrar/registrar (форма не отправляется, но всё регается)
+Registrar id: registrar123
+Registrar name: myfirstapp
+Registrar password: myfirstapp1
+Получение OAuth client ID, по ID приложения: https://oauth.yandex.ru/authorize?response_type=token&client_id=id123
+OAuth client ID: OAuth_client_ID
+
+Получить ПДД-токен на странице управления токеном, указав идентификатор созданного регистратора: https://pddimp.yandex.ru/api2/registrar/get_token
+token
+*/
+
+require_once 'vendor/autoload.php';
+require_once 'HTTP/Request2.php';
+
+/**
+ * Class YandexDNS работа с DNS
+ */
+class YandexDNS {
+
+ /**
+  * @var токен
+  */
+ public $PddToken;
+
+ /**
+  * добавить домен в сервис
+  * @param $domain
+  * @return true
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function admin_adddomain($domain) {
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/register', HTTP_Request2::METHOD_POST);
+  $request->setHeader(array('PddToken' => $this->PddToken));
+  $request->setBody('domain=' . $domain);
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+  return $body->status == 'added';
+ }
+
+ /**
+  * удалить все DNS-записи
+  * @param $domain
+  * @return int кол-во удаленных записей
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function admin_dellalldns($domain) {
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/list?domain=' . $domain, HTTP_Request2::METHOD_GET);
+  $request->setHeader(array('PddToken' => $this->PddToken));
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+
+  $result = 0;
+  foreach ($body->records as $item) {
+   $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/del', HTTP_Request2::METHOD_POST);
+   $request->setHeader(array('PddToken' => $this->PddToken));
+   $request->setBody('domain=' . $domain . '&record_id=' . $item->record_id);
+   $responce = $request->send();
+   $body = json_decode($responce->getBody());
+   var_dump($body);
+   if ($body->success == 'ok') {
+    $result += 1;
+   }
+  }
+  return $result;
+ }
+
+ /**
+  * добавить DNS-запись
+  * @param $domain
+  * @param $options
+  * @return true
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function admin_adddns($domain, $options) {
+  $options_string = '';
+  foreach ($options as $key => $value) {
+   $options_string .= '&' . $key . '=' . $value;
+  }
+
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/add', HTTP_Request2::METHOD_POST);
+  $request->setHeader(array('PddToken' => $this->PddToken));
+  $request->setBody('domain=' . $domain . $options_string);
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+  return $body->success == 'ok';
+ }
+
+ /**
+  * @var токен
+  */
+ public $Authorization;
+
+ /**
+  * добавить домен в сервис
+  * @param $domain
+  * @return true
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function registrant_adddomain($domain) {
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/registrar/domain/register', HTTP_Request2::METHOD_POST);
+  $request->setHeader(array('PddToken' => $this->PddToken, 'Authorization' => $this->Authorization));
+  $request->setBody('domain=' . $domain);
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+  return $body->status == 'added';
+ }
+
+ /**
+  * удалить все DNS-записи
+  * @param $domain
+  * @return int кол-во удаленных записей
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function registrant_dellalldns($domain) {
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/registrar/dns/list?domain=' . $domain, HTTP_Request2::METHOD_GET);
+  $request->setHeader(array('PddToken' => $this->PddToken, 'Authorization' => $this->Authorization));
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+
+  $result = 0;
+  foreach ($body->records as $item) {
+   $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/registrar/dns/del', HTTP_Request2::METHOD_POST);
+   $request->setHeader(array('PddToken' => $this->PddToken, 'Authorization' => $this->Authorization));
+   $request->setBody('domain=' . $domain . '&record_id=' . $item->record_id);
+   $responce = $request->send();
+   $body = json_decode($responce->getBody());
+   var_dump($body);
+   if ($body->success == 'ok') {
+    $result += 1;
+   }
+  }
+  return $result;
+ }
+
+ /**
+  * добавить DNS-запись
+  * @param $domain
+  * @param $options
+  * @return true
+  * @throws Exception
+  * @throws HTTP_Request2_LogicException
+  */
+ public function registrar_adddns($domain, $options) {
+  $options_string = '';
+  foreach ($options as $key => $value) {
+   $options_string .= '&' . $key . '=' . $value;
+  }
+
+  $request = new HTTP_Request2('https://pddimp.yandex.ru/api2/registrar/dns/add', HTTP_Request2::METHOD_POST);
+  $request->setHeader(array('PddToken' => $this->PddToken, 'Authorization' => $this->Authorization));
+  $request->setBody('domain=' . $domain . $options_string);
+  $responce = $request->send();
+  $body = json_decode($responce->getBody());
+  var_dump($body);
+  return $body->success == 'ok';
+ }
+
+}
+
+//работа с YandexDNS (как регистратор)
+$b = new YandexDNS();
+$b->PddToken = '123';
+$b->Authorization = '123';
+$b->registrant_adddomain('tratata.ru');
+$b->registrant_dellalldns('tratata.ru');
+$b->registrar_adddns('tratata.ru', array('type' => 'A', 'content' => '127.0.0.2'));
---
+++ YandexDNS/YandexDNS_Test.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'HTTP/Request2.php';
+$domain = 'tratata.ru';
+
+
+//require_once 'HTTP/Request2.php';
+//require_once 'HTTP/Request2/CookieJar.php';
+//require_once 'HTTP/Request2/MultipartBody.php';
+//use HTTP_Request2;
+
+
+//Получение списка доменов
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/domains?on_page=20', HTTP_Request2::METHOD_GET);
+$request->setHeader(array('PddToken' => 'token123'));
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Получение списка доменов');
+var_dump($body);
+
+
+//Подключить домен
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/register', HTTP_Request2::METHOD_POST);
+$request->setHeader(array('PddToken' => 'token123'));
+$request->setBody('domain=' . $domain);
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Подключить домен');
+var_dump($body);
+
+
+//Получить статус подключения домена
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/registration_status?domain=' . $domain, HTTP_Request2::METHOD_GET);
+$request->setHeader(array('PddToken' => 'token123'));
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Получить статус подключения домена');
+var_dump($body);
+
+
+//Получить настройки домена
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/details?domain=' . $domain, HTTP_Request2::METHOD_GET);
+$request->setHeader(array('PddToken' => 'token123'));
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Получить настройки домена');
+var_dump($body);
+
+
+//Удалить домен
+//$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/domain/delete', HTTP_Request2::METHOD_POST);
+//$request->setHeader(array('PddToken' => 'token123'));
+//$request->setBody('domain=' . $domain);
+//$responce = $request->send();
+//$headers = $responce->getHeader();
+//$body = json_decode($responce->getBody());
+//
+//var_dump($headers);
+//var_dump('Удалить домен');
+//var_dump($body);
+
+
+//Получить DNS-записи домена
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/list?domain=' . $domain, HTTP_Request2::METHOD_GET);
+$request->setHeader(array('PddToken' => 'token123'));
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Получить DNS-записи домена');
+var_dump($body);
+
+
+//Добавить DNS-запись
+$types = array('SRV', 'TXT', 'NS', 'MX', 'SOA', 'A', 'AAAA', 'CNAME');
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/add', HTTP_Request2::METHOD_POST);
+$request->setHeader(array('PddToken' => 'token123'));
+$request->setBody('domain=' . $domain . '&type=' . $types[5] . '&content=127.0.0.1');
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Добавить DNS-запись');
+var_dump($body);
+
+
+//Редактировать DNS-запись
+$types = array('SRV', 'TXT', 'NS', 'MX', 'SOA', 'A', 'AAAA', 'CNAME');
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/edit', HTTP_Request2::METHOD_POST);
+$request->setHeader(array('PddToken' => 'token123'));
+$request->setBody('domain=' . $domain . '&type=' . $types[5] . '&record_id=33767611&content=127.0.0.2');
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Редактировать DNS-запись');
+var_dump($body);
+
+
+//Удалить DNS-запись
+$types = array('SRV', 'TXT', 'NS', 'MX', 'SOA', 'A', 'AAAA', 'CNAME');
+$request = new HTTP_Request2('https://pddimp.yandex.ru/api2/admin/dns/del', HTTP_Request2::METHOD_POST);
+$request->setHeader(array('PddToken' => 'token123'));
+$request->setBody('domain=' . $domain . '&record_id=33767611');
+$responce = $request->send();
+$headers = $responce->getHeader();
+$body = json_decode($responce->getBody());
+
+//var_dump($headers);
+var_dump('Удалить DNS-запись');
+var_dump($body);
