d620bf575ec3e6df6f073710ea46397feabe04b8
traster100 <traster100@gmail.com>
2018-11-14 07:41:19 +0300

    Add files via upload

---
+++ BrowserDeviceOsDetector.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Sinergi\BrowserDetector\Browser;
+
+$browser = new Browser();
+var_dump('Browser: ' . $browser->getName());
+
+//===================================================
+
+use Sinergi\BrowserDetector\Os;
+
+$os = new Os();
+var_dump('Os: ' . $os->getName());
+
+//===================================================
+
+use Sinergi\BrowserDetector\Device;
+
+$device = new Device();
+var_dump('Device: ' . $device->getName());
