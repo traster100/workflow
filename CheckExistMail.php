7d067c28adc4c156ef5cf10173ecca6d41e408b2
u <u@u.com>
2019-04-15 10:34:10 +0300

    удалил почту

--- CheckExistMail.php
+++ CheckExistMail.php
@@ -11,124 +11,124 @@
 
 class Emails {
 
- private $alfavit = array(
-  "а" => "a",
-  "б" => "b",
-  "в" => "v",
-  "г" => "g",
-  "д" => "d",
-  "е" => "e",
-  "ё" => "yo",
-  "ж" => "j",
-  "з" => "z",
-  "и" => "i",
-  "й" => "i",
-  "к" => "k",
-  "л" => "l",
-  "м" => "m",
-  "н" => "n",
-  "о" => "o",
-  "п" => "p",
-  "р" => "r",
-  "с" => "s",
-  "т" => "t",
-  "у" => "y",
-  "ф" => "f",
-  "х" => "h",
-  "ц" => "c",
-  "ч" => "ch",
-  "ш" => "sh",
-  "щ" => "sh",
-  "ы" => "i",
-  "э" => "e",
-  "ю" => "u",
-  "я" => "ya",
-  "А" => "A",
-  "Б" => "B",
-  "В" => "V",
-  "Г" => "G",
-  "Д" => "D",
-  "Е" => "E",
-  "Ё" => "Yo",
-  "Ж" => "J",
-  "З" => "Z",
-  "И" => "I",
-  "Й" => "I",
-  "К" => "K",
-  "Л" => "L",
-  "М" => "M",
-  "Н" => "N",
-  "О" => "O",
-  "П" => "P",
-  "Р" => "R",
-  "С" => "S",
-  "Т" => "T",
-  "У" => "Y",
-  "Ф" => "F",
-  "Х" => "H",
-  "Ц" => "C",
-  "Ч" => "Ch",
-  "Ш" => "Sh",
-  "Щ" => "Sh",
-  "Ы" => "I",
-  "Э" => "E",
-  "Ю" => "U",
-  "Я" => "Ya",
-  "ь" => "",
-  "Ь" => "",
-  "ъ" => "",
-  "Ъ" => ""
- );
+  private $alfavit = array(
+    "а" => "a",
+    "б" => "b",
+    "в" => "v",
+    "г" => "g",
+    "д" => "d",
+    "е" => "e",
+    "ё" => "yo",
+    "ж" => "j",
+    "з" => "z",
+    "и" => "i",
+    "й" => "i",
+    "к" => "k",
+    "л" => "l",
+    "м" => "m",
+    "н" => "n",
+    "о" => "o",
+    "п" => "p",
+    "р" => "r",
+    "с" => "s",
+    "т" => "t",
+    "у" => "y",
+    "ф" => "f",
+    "х" => "h",
+    "ц" => "c",
+    "ч" => "ch",
+    "ш" => "sh",
+    "щ" => "sh",
+    "ы" => "i",
+    "э" => "e",
+    "ю" => "u",
+    "я" => "ya",
+    "А" => "A",
+    "Б" => "B",
+    "В" => "V",
+    "Г" => "G",
+    "Д" => "D",
+    "Е" => "E",
+    "Ё" => "Yo",
+    "Ж" => "J",
+    "З" => "Z",
+    "И" => "I",
+    "Й" => "I",
+    "К" => "K",
+    "Л" => "L",
+    "М" => "M",
+    "Н" => "N",
+    "О" => "O",
+    "П" => "P",
+    "Р" => "R",
+    "С" => "S",
+    "Т" => "T",
+    "У" => "Y",
+    "Ф" => "F",
+    "Х" => "H",
+    "Ц" => "C",
+    "Ч" => "Ch",
+    "Ш" => "Sh",
+    "Щ" => "Sh",
+    "Ы" => "I",
+    "Э" => "E",
+    "Ю" => "U",
+    "Я" => "Ya",
+    "ь" => "",
+    "Ь" => "",
+    "ъ" => "",
+    "Ъ" => ""
+  );
 
 
 //имя
 //фамилия
 //отчество
- public function generate($name, $surname, $patronymic, $domain) {
-  $name = strtolower(strtr($name, $this->alfavit));
-  $surname = strtolower(strtr($surname, $this->alfavit));
-  $patronymic = strtolower(strtr($patronymic, $this->alfavit));
+  public function generate($name, $surname, $patronymic, $domain) {
+    $name = strtolower(strtr($name, $this->alfavit));
+    $surname = strtolower(strtr($surname, $this->alfavit));
+    $patronymic = strtolower(strtr($patronymic, $this->alfavit));
 
-  $variants = array(
-   $name,
-   $surname,
+    $variants = array(
+      $name,
+      $surname,
 
-   $name . '.' . $surname,
-   $surname . '.' . $name,
+      $name . '.' . $surname,
+      $surname . '.' . $name,
 
-   substr($name, 0, 1) . '.' . $surname,
-   $name . '.' . substr($surname, 0, 1),
+      substr($name, 0, 1) . '.' . $surname,
+      $name . '.' . substr($surname, 0, 1),
 
-   substr($surname, 0, 1) . '.' . $name,
-   $surname . '.' . substr($name, 0, 1),
+      substr($surname, 0, 1) . '.' . $name,
+      $surname . '.' . substr($name, 0, 1),
 
-   'marketing' . '.' . $surname,
-   'marketing' . '.' . $name,
+      'marketing' . '.' . $surname,
+      'marketing' . '.' . $name,
 
-   $name . '.' . 'marketing',
-   $surname . '.' . 'marketing',
+      $name . '.' . 'marketing',
+      $surname . '.' . 'marketing',
 
-   substr($name, 0, 1) . '.' . $surname . '.' . 'marketing',
-   $name . '.' . substr($surname, 0, 1) . '.' . 'marketing',
+      substr($name, 0, 1) . '.' . $surname . '.' . 'marketing',
+      $name . '.' . substr($surname, 0, 1) . '.' . 'marketing',
 
-   substr($surname, 0, 1) . '.' . $name . '.' . 'marketing',
-   $surname . '.' . substr($name, 0, 1) . '.' . 'marketing',
+      substr($surname, 0, 1) . '.' . $name . '.' . 'marketing',
+      $surname . '.' . substr($name, 0, 1) . '.' . 'marketing',
 
-   'marketing' . '.' . substr($name, 0, 1) . '.' . $surname,
-   'marketing' . '.' . $name . '.' . substr($surname, 0, 1),
+      'marketing' . '.' . substr($name, 0, 1) . '.' . $surname,
+      'marketing' . '.' . $name . '.' . substr($surname, 0, 1),
 
-   'marketing' . '.' . substr($surname, 0, 1) . '.' . $name,
-   'marketing' . '.' . $surname . '.' . substr($name, 0, 1),
+      'marketing' . '.' . substr($surname, 0, 1) . '.' . $name,
+      'marketing' . '.' . $surname . '.' . substr($name, 0, 1),
 
-  );
+    );
 
-  foreach ($variants as &$variant) {
-   $variant = $variant . '@' . $domain;
+    foreach ($variants as &$variant) {
+      $variant = $variant . '@' . $domain;
+    }
+
+    return $variants;
+
   }
-
-  return $variants;
-
- }
 
 
 }
@@ -136,14 +136,14 @@ class Emails {
 $a = new Emails();
 
 $fios = array(
- array('Иван', 'Иванов', 'Иванович', 'eldorado.ru'),
- array('Петр', 'Петров', 'Петрович', 'tehnosila.ru'),
- array('Сидр', 'Сидоров', 'Сидорович', 'mvideo.ru'),
+  array('Иван', 'Иванов', 'Иванович', 'eldorado.ru'),
+  array('Петр', 'Петров', 'Петрович', 'tehnosila.ru'),
+  array('Сидр', 'Сидоров', 'Сидорович', 'mvideo.ru'),
 );
 
 $result = array();
 foreach ($fios as $fio) {
- $result[] = $a->generate($fio[0], $fio[1], $fio[2], $fio[3]);
+  $result[] = $a->generate($fio[0], $fio[1], $fio[2], $fio[3]);
 }
 
 //https://github.com/hbattat/verifyEmail
@@ -151,19 +151,19 @@ include 'src/VerifyEmail.php';
 
 
 foreach ($result as $v1) {
- foreach ($v1 as $v2) {
-  $v2 = 'traster100@gmail.com';
-  echo $v2 . ' <br>';
+  foreach ($v1 as $v2) {
+    $v2 = 'NNN@gmail.com';
+    echo $v2 . ' <br>';
 
-  $ve = new hbattat\VerifyEmail($v2, 'traster100@gmail.com');
-  var_dump($ve->verify());
-  echo '<pre>';
-  print_r($ve->get_debug());
+    $ve = new hbattat\VerifyEmail($v2, 'NNN@gmail.com');
+    var_dump($ve->verify());
+    echo '<pre>';
+    print_r($ve->get_debug());
 
-  exit;
+    exit;
 
- }
- echo ' <br>';
+  }
+  echo ' <br>';
 }
 
 
--- ParsingPostingVK/3_send_message.php
+++ ParsingPostingVK/3_send_message.php
@@ -15,7 +15,7 @@ require_once 'vendor/curl.php';
 require_once 'vendor/db.php';
 
 function __debug($id, $profile, $status) {
- file_put_contents('3_send_message.log', $id . ' : ' . $profile . ' : ' . $status . "\n", FILE_USE_INCLUDE_PATH | FILE_APPEND | LOCK_EX);
+  file_put_contents('3_send_message.log', $id . ' : ' . $profile . ' : ' . $status . "\n", FILE_USE_INCLUDE_PATH | FILE_APPEND | LOCK_EX);
 }
 
 $sleep = 5;
@@ -26,103 +26,103 @@ echo date('H:i:s') . '<br><br>';
 for ($i = 0; $i < 200; $i++) {
 
 //стартуем браузер
- $driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', DesiredCapabilities::chrome(), 5000);
+  $driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', DesiredCapabilities::chrome(), 5000);
 
 //аккаунт с которого постим
- $account = $db->account_get();
+  $account = $db->account_get();
 //профиль кому постим
- $profile = $db->profile_get();
+  $profile = $db->profile_get();
 
 //тестовый аккаунт
-//$profile['url'] = 'https://vk.com/traster100';
+//$profile['url'] = 'https://vk.com/NNN';
 //$profile['id'] = 000;
 
 //текст
- $address = 'Москва, 21 августа, 13-00, встреча';
- $text = file_get_contents('messages.txt', FILE_USE_INCLUDE_PATH);
- $text = preg_replace("~(NNN)~isu", $address, $text);
- $texts = preg_split("~={10}~isu", $text);
- $message = trim($texts[array_rand($texts)]);
+  $address = 'Москва, 21 августа, 13-00, встреча';
+  $text = file_get_contents('messages.txt', FILE_USE_INCLUDE_PATH);
+  $text = preg_replace("~(NNN)~isu", $address, $text);
+  $texts = preg_split("~={10}~isu", $text);
+  $message = trim($texts[array_rand($texts)]);
 
- if (!$account OR !$profile) {
-  $driver->quit();
-  exit ('кончились аккаунты или профиля');
- }
+  if (!$account OR !$profile) {
+    $driver->quit();
+    exit ('кончились аккаунты или профиля');
+  }
 
- echo '<hr>' . $i . '<br>';
- echo 'аккаунт : ' . $account['id'] . ' : ' . $account['login'] . ' : ' . $account['password'] . '<br>';
- echo 'профиль : ' . $profile['id'] . ' : ' . '<a target="_blank" href="' . $profile['url'] . '">' . $profile['url'] . '</a>' . '<br>';
+  echo '<hr>' . $i . '<br>';
+  echo 'аккаунт : ' . $account['id'] . ' : ' . $account['login'] . ' : ' . $account['password'] . '<br>';
+  echo 'профиль : ' . $profile['id'] . ' : ' . '<a target="_blank" href="' . $profile['url'] . '">' . $profile['url'] . '</a>' . '<br>';
 //    print_r($account);
 //    print_r($profile);
 
 //открываем урл
- try {
-  $driver->get($profile['url']);
- } catch (\Exception $e) {
-  echo $e->getMessage();
-  $driver->quit();
-  continue;
- }
- sleep($sleep);
+  try {
+    $driver->get($profile['url']);
+  } catch (\Exception $e) {
+    echo $e->getMessage();
+    $driver->quit();
+    continue;
+  }
+  sleep($sleep);
 
 //логин
- try {
-  $driver->findElement(WebDriverBy::id("quick_email"))->sendKeys($account['login']);
-  $driver->findElement(WebDriverBy::id("quick_pass"))->sendKeys($account['password']);
-  $driver->findElement(WebDriverBy::id('quick_login_button'))->click();
+  try {
+    $driver->findElement(WebDriverBy::id("quick_email"))->sendKeys($account['login']);
+    $driver->findElement(WebDriverBy::id("quick_pass"))->sendKeys($account['password']);
+    $driver->findElement(WebDriverBy::id('quick_login_button'))->click();
 //        $driver->findElement(WebDriverBy::id("index_email"))->sendKeys($account['login']);
 //        $driver->findElement(WebDriverBy::id("index_pass"))->sendKeys($account['password']);
 //        $driver->findElement(WebDriverBy::id('index_login_button'))->click();
- } catch (\Exception $e) {
-  echo $e->getMessage();
-  $driver->quit();
-  continue;
- }
- sleep($sleep);
+  } catch (\Exception $e) {
+    echo $e->getMessage();
+    $driver->quit();
+    continue;
+  }
+  sleep($sleep);
 
 
 //убрать все попапы
- for ($i1 = 0; $i1 < 10; $i1++) {
-  try {
-   $driver->getKeyboard()->sendKeys(WebDriverKeys::ESCAPE);
-  } catch (\Exception $e) {
-   continue;
+  for ($i1 = 0; $i1 < 10; $i1++) {
+    try {
+      $driver->getKeyboard()->sendKeys(WebDriverKeys::ESCAPE);
+    } catch (\Exception $e) {
+      continue;
+    }
   }
- }
 
 //забираем html страницы
- try {
-  $page = $driver->getPageSource();
- } catch (\Exception $e) {
-  echo $e->getMessage();
-  $driver->quit();
-  continue;
- }
- $html = new \simple_html_dom();
- $html->load($page);
+  try {
+    $page = $driver->getPageSource();
+  } catch (\Exception $e) {
+    echo $e->getMessage();
+    $driver->quit();
+    continue;
+  }
+  $html = new \simple_html_dom();
+  $html->load($page);
 
 //аккаунт забанен
- $sel1 = 'div.login_blocked_centered';
- $sel2 = '#message';
- if (
-  (
-   $html->find($sel1, 0)
-   AND
-   preg_match("~Эта страница была заморожена за рассылку от Вашего лица таких~isu", $html->find($sel1, 0)->plaintext) == 1
-  )
-  OR
-  (
-   $html->find($sel2, 0)
-   AND
-   preg_match("~Не удается войти~isu", $html->find($sel2, 0)->plaintext) == 1
-  )
- ) {
-  $db->account_ban($account['id']);
-  __debug($account['id'], '', 'аккаунт забанен');
-  echo 'аккаунт забанен';
-  $driver->quit();
-  continue;
- }
+  $sel1 = 'div.login_blocked_centered';
+  $sel2 = '#message';
+  if (
+    (
+      $html->find($sel1, 0)
+      AND
+      preg_match("~Эта страница была заморожена за рассылку от Вашего лица таких~isu", $html->find($sel1, 0)->plaintext) == 1
+    )
+    OR
+    (
+      $html->find($sel2, 0)
+      AND
+      preg_match("~Не удается войти~isu", $html->find($sel2, 0)->plaintext) == 1
+    )
+  ) {
+    $db->account_ban($account['id']);
+    __debug($account['id'], '', 'аккаунт забанен');
+    echo 'аккаунт забанен';
+    $driver->quit();
+    continue;
+  }
 
 //0-не отправлялось
 //1-забанен
@@ -132,109 +132,109 @@ for ($i = 0; $i < 200; $i++) {
 //5-отправлено сообщение
 
 //профиль забанен
- $sel = '.profile_blocked';
- if (
-  $html->find($sel, 0)
-  AND
-  preg_match("~Мы обнаружили на странице(.*?)подозрительную активность~isu", $html->find($sel, 0)->plaintext) == 1
- ) {
-  $db->profile_update($profile['id'], 1);
-  __debug($profile['id'], $profile['url'], 1);
-  echo 'профиль забанен';
-  $driver->quit();
-  continue;
- }
+  $sel = '.profile_blocked';
+  if (
+    $html->find($sel, 0)
+    AND
+    preg_match("~Мы обнаружили на странице(.*?)подозрительную активность~isu", $html->find($sel, 0)->plaintext) == 1
+  ) {
+    $db->profile_update($profile['id'], 1);
+    __debug($profile['id'], $profile['url'], 1);
+    echo 'профиль забанен';
+    $driver->quit();
+    continue;
+  }
 
 //профиль удален
- $sel1 = '.profile_deleted';
- $sel2 = '.profile_deleted_text';
- $sel3 = 'div.message_page div.body';
- if (
-  (
-   $html->find($sel1, 0)
-   AND
-   preg_match("~Страница удалена~isu", $html->find($sel1, 0)->plaintext) == 1
-   AND
-   $html->find($sel2, 0)
-   AND
-   preg_match("~Страница пользователя удалена~isu", $html->find($sel2, 0)->plaintext) == 1
-  )
-  OR
-  (
-   $html->find($sel3, 0)
-   AND
-   preg_match("~Страница удалена либо ещё не создана~isu", $html->find($sel3, 0)->plaintext) == 1
-  )
- ) {
-  $db->profile_update($profile['id'], 2);
-  __debug($profile['id'], $profile['url'], 2);
-  echo 'профиль удален';
-  $driver->quit();
-  continue;
- }
+  $sel1 = '.profile_deleted';
+  $sel2 = '.profile_deleted_text';
+  $sel3 = 'div.message_page div.body';
+  if (
+    (
+      $html->find($sel1, 0)
+      AND
+      preg_match("~Страница удалена~isu", $html->find($sel1, 0)->plaintext) == 1
+      AND
+      $html->find($sel2, 0)
+      AND
+      preg_match("~Страница пользователя удалена~isu", $html->find($sel2, 0)->plaintext) == 1
+    )
+    OR
+    (
+      $html->find($sel3, 0)
+      AND
+      preg_match("~Страница удалена либо ещё не создана~isu", $html->find($sel3, 0)->plaintext) == 1
+    )
+  ) {
+    $db->profile_update($profile['id'], 2);
+    __debug($profile['id'], $profile['url'], 2);
+    echo 'профиль удален';
+    $driver->quit();
+    continue;
+  }
 
 //наличие кнопки 'Написать сообщение'
- $sel = '.profile_btn_cut_left';
- if (
-  $html->find($sel, 0)
-  AND
-  preg_match("~Написать сообщение~isu", $html->find($sel, 0)->plaintext) == 1
- ) {
-  echo 'есть кнопка "Написать сообщение".';
- } else {
-  $db->profile_update($profile['id'], 4);
-  __debug($profile['id'], $profile['url'], 4);
-  echo 'нет кнопки "Написать сообщение".';
-  $driver->quit();
-  continue;
- }
+  $sel = '.profile_btn_cut_left';
+  if (
+    $html->find($sel, 0)
+    AND
+    preg_match("~Написать сообщение~isu", $html->find($sel, 0)->plaintext) == 1
+  ) {
+    echo 'есть кнопка "Написать сообщение".';
+  } else {
+    $db->profile_update($profile['id'], 4);
+    __debug($profile['id'], $profile['url'], 4);
+    echo 'нет кнопки "Написать сообщение".';
+    $driver->quit();
+    continue;
+  }
 
 
 //постим
- try {
+  try {
 //клик на кнопке Написать сообщение
-  $driver->findElement(WebDriverBy::cssSelector($sel))->click();
-  sleep($sleep);
-  $driver->findElement(WebDriverBy::id("mail_box_editable"))->sendKeys($message);
-  sleep($sleep);
-  $driver->findElement(WebDriverBy::id('mail_box_send'))->click();
- } catch
- (\Exception $e) {
-  echo $e->getMessage();
-  __debug($profile['id'], $profile['url'], 'попап не отправился');
-  echo 'попап не отправился';
-  $driver->quit();
-  continue;
- }
+    $driver->findElement(WebDriverBy::cssSelector($sel))->click();
+    sleep($sleep);
+    $driver->findElement(WebDriverBy::id("mail_box_editable"))->sendKeys($message);
+    sleep($sleep);
+    $driver->findElement(WebDriverBy::id('mail_box_send'))->click();
+  } catch
+  (\Exception $e) {
+    echo $e->getMessage();
+    __debug($profile['id'], $profile['url'], 'попап не отправился');
+    echo 'попап не отправился';
+    $driver->quit();
+    continue;
+  }
 
 //ловим попап "Сообщение отправлено"
- for ($i2 = 0; $i2 <= 20; $i2++) {
+  for ($i2 = 0; $i2 <= 20; $i2++) {
 //забираем html страницы
-  try {
-   $page = $driver->getPageSource();
-  } catch (\Exception $e) {
-   echo $e->getMessage();
-   continue;
-  }
-  $html = new \simple_html_dom();
-  $html->load($page);
+    try {
+      $page = $driver->getPageSource();
+    } catch (\Exception $e) {
+      echo $e->getMessage();
+      continue;
+    }
+    $html = new \simple_html_dom();
+    $html->load($page);
 
 //панелька "Сообщение отправлено"
-  $result = $html->find('div.top_result_baloon div.top_result_header', 0)->plaintext;
-  var_dump($result);
+    $result = $html->find('div.top_result_baloon div.top_result_header', 0)->plaintext;
+    var_dump($result);
 
-  if (preg_match("~Сообщение отправлено~isu", $result) == 1) {
-   $db->account_update($account['id']);
-   $db->profile_update($profile['id'], 5);
-   __debug($profile['id'], $profile['url'], 5);
-   $driver->quit();
-   break;
+    if (preg_match("~Сообщение отправлено~isu", $result) == 1) {
+      $db->account_update($account['id']);
+      $db->profile_update($profile['id'], 5);
+      __debug($profile['id'], $profile['url'], 5);
+      $driver->quit();
+      break;
+    }
+
+    usleep(100000); // 0.1 секунды
+    $html->clear();
+    unset($html);
   }
-
-  usleep(100000); // 0.1 секунды
-  $html->clear();
-  unset($html);
- }
 
 }
 
