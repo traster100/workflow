c8923cd371f3d9a4862553cba660ce868927ea9c
u <u@u.com>
2020-01-12 23:30:56 +0300

    дополнил

--- PHPMailer.php
+++ PHPMailer.php
@@ -1,5 +1,8 @@
 <?php
 
+//composer require phpmailer/phpmailer
+
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -11,44 +14,44 @@ require 'vendor/autoload.php';
 
 function send_email_smtp($email, $message) {
 
-  $mail = new PHPMailer(true); // Passing `true` enables exceptions
+    $mail = new PHPMailer(true); // Passing `true` enables exceptions
 
-  try {
+    try {
 
-    //Server settings
-    $mail->SMTPDebug = 4; // Enable verbose debug output
-    $mail->isSMTP(); // Set mailer to use SMTP
-    $mail->Host = 'mail.domain.com'; // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = 'Username'; // SMTP username
-    $mail->Password = 'Password'; // SMTP password
-    $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587; // TCP port to connect to
-    $mail->CharSet = 'UTF-8';
+        //Server settings
+        $mail->SMTPDebug = 4; // Enable verbose debug output
+        $mail->isSMTP(); // Set mailer to use SMTP
+        $mail->Host = 'mail.domain.com'; // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'Username'; // SMTP username
+        $mail->Password = 'Password'; // SMTP password
+        $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587; // TCP port to connect to
+        $mail->CharSet = 'UTF-8';
 
-    //Recipients
-    $mail->setFrom('robot@domain.com');
-    $mail->addAddress($email); // Add a recipient
-    $mail->addReplyTo('robot@domain.com');
-    $mail->addCC('robot@domain.com');
-    $mail->addBCC('robot@domain.com');
+        //Recipients
+        $mail->setFrom('robot@domain.com');
+        $mail->addAddress($email); // Add a recipient
+        $mail->addReplyTo('robot@domain.com');
+        $mail->addCC('robot@domain.com');
+        $mail->addBCC('robot@domain.com');
 
-    //Content
-    $mail->isHTML(true); // Set email format to HTML
-    $mail->Subject = 'Проверка(smtp) ' . date('H:i:s');
-    $mail->Body = $message;
-    $mail->AltBody = $message;
+        //Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = 'Проверка(smtp) ' . date('H:i:s');
+        $mail->Body = $message;
+        $mail->AltBody = $message;
 
-    $mail->send();
-  } catch (Exception $e) {
+        $mail->send();
+    } catch (Exception $e) {
 //    echo $mail->ErrorInfo;
-    echo 'не отправлено';
-  }
+        echo 'не отправлено';
+    }
 }
 
 foreach (['mail1@mail.com', 'mail2@mail.com'] as $v) {
-  send_email_smtp($v, 'код 123456');
-  echo $v . ' отправлено' . '<br>';
+    send_email_smtp($v, 'код 123456');
+    echo $v . ' отправлено' . '<br>';
 }
 
 
@@ -56,15 +59,15 @@ foreach (['mail1@mail.com', 'mail2@mail.com'] as $v) {
 
 foreach (['mail1@mail.com', 'mail2@mail.com'] as $v) {
 
-  $headers =
-    'From: robot@domain.com' . "\r\n"
-    . 'Reply-To: robot@domain.com' . "\r\n"
-    . 'Cc: robot@domain.com' . "\r\n"
-    . 'Bcc: robot@domain.com' . "\r\n"
-    . 'X-Mailer: PHP/' . phpversion();
+    $headers =
+        'From: robot@domain.com' . "\r\n"
+        . 'Reply-To: robot@domain.com' . "\r\n"
+        . 'Cc: robot@domain.com' . "\r\n"
+        . 'Bcc: robot@domain.com' . "\r\n"
+        . 'X-Mailer: PHP/' . phpversion();
 
-  mail($v, 'Проверка(mail) ' . date('H:i:s'), 'tratata', $headers);
+    mail($v, 'Проверка(mail) ' . date('H:i:s'), 'tratata', $headers);
 
-  echo $v . ' отправлено' . '<br>';
+    echo $v . ' отправлено' . '<br>';
 
 }
